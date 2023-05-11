e1bebfa01dc043dd19dfce0eedb25ef80649363a
Cédric FONSAT <79462084+CedricFonsat@users.noreply.github.com>
2023-05-11 14:20:35 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-module.exports = {
-    mongodb: {
-      url: "mongodb+srv://collect7:admin@cluster0.cquaia9.mongodb.net/calendar?retryWrites=true&w=majority"
-      , // URL de connexion à MongoDB
-    },
-  };
-  
